7ba65eb09a48e75e48288e0688aca3198a11d0f0
Tim Shichalin <shichatim@gmail.com>
2025-04-06 17:40:37 -0700

    allows adding courses to log

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
+import { Log } from "./data_handling/log";
 
 function App() {
+
+    const [log, setLog] = useState(new Log());
+    const [courseNames, setCourseNames] = useState("");
+    const [nameInputValue, setNameInputValue] = useState("");
+
     return (
         <div className="App">
             <h1>Disc golf pad</h1>
+            <p>{courseNames}</p>
+            <input
+                type="text"
+                value={nameInputValue}
+                onChange={(event) => {
+                    setNameInputValue(event.target.value);
+                }}
+            />
+            <button onClick={() => {
+                log.addCourse(nameInputValue);
+                setNameInputValue("");
+
+                setCourseNames(log.getCourseNames());
+            }}>
+                Add course
+            </button>
         </div>
     );
 }
---
+++ client/src/data_handling/log.js
@@ -0,0 +1,35 @@
+import { Course } from "./course";
+
+export class Log {
+    constructor (courses) {
+        this.courses = {};
+        if(courses) {
+            this.courses = courses;
+        }
+    }
+
+    addCourse = (nameOrCourse) => {
+        // If the name was given
+        if(typeof nameOrCourse === "string") {
+            // Make sure this name isn't already used
+            if(this.courses[nameOrCourse]) {
+                return false;
+            }
+        }
+        // If the course was given
+        else {
+            // Make sure the name of the given course is
+            // not already used
+            if(this.courses[nameOrCourse.name]) {
+                return false;
+            }
+        }
+        let newCourse = new Course(nameOrCourse);
+        this.courses[nameOrCourse] = newCourse;
+        return newCourse;
+    }
+
+    getCourseNames = () => {
+        return Object.keys(this.courses);
+    }
+}
