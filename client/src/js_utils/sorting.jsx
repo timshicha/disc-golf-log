c601c762a3394fec13159ac92386615ff03c9861
Tim Shichalin <shichatim@gmail.com>
2025-05-01 22:36:34 -0700

    sort courses that were never modified (since update) to end instead of start

--- client/src/js_utils/sorting.jsx
+++ client/src/js_utils/sorting.jsx
@@ -8,12 +8,11 @@ const compareStrings = (string1, string2) => {
     return 1;
 }
 
-// -1 if first date comes first
+// -1 if first date comes first or if it's null
 // 0 if same
 // 1 if the second date comes first (or if first is null)
 const compareDates = (date1, date2) => {
-    if(!date1) return 1;
-    if(date1 < date2) return -1;
+    if(!date1 || date1 < date2) return -1;
     if(date1 === date2) return 0;
     return 1;
 }
