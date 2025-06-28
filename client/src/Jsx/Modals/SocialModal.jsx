8a250527c8838ed2a91dc5258884a726eeb76f64
Tim Shichalin <shichatim@gmail.com>
2025-06-27 23:54:04 -0800

    social page loads conditionally based on if the user is logged in

--- client/src/Jsx/Modals/SocialModal.jsx
+++ client/src/Jsx/Modals/SocialModal.jsx
@@ -3,14 +3,18 @@ import ModalTitle from "./ModalComponents/ModalTitle";
 
 const SocialModal = (props) => {
 
-    const username = localStorage.getItem("username") || "";
+    const username = localStorage.getItem("username") || null;
 
     return (
         <LargeModal {...props}>
             <ModalTitle>Social</ModalTitle>
+            {username ?
             <div className="bg-gray-light w-[95%] mx-auto h-[50px]">
                 <div className="w-[fit-content] text-desc text-gray-mild ml-[10px]">{username}</div>
-            </div>
+            </div> :
+            <div className="text-desc text-gray-mild mt-[50%]">
+                Log in to view social page.
+            </div>}
         </LargeModal>
     );
 }
