69913e0b72f80d533e85962150964429e55613fa
Tim Shichalin <shichatim@gmail.com>
2025-07-09 13:50:04 -0700

    social round format fixes, shows total for each round

--- client/src/Jsx/Components/SocialCourse.jsx
+++ client/src/Jsx/Components/SocialCourse.jsx
@@ -2,13 +2,14 @@ import React, { useEffect, useState } from "react";
 import { httpGetUserCourse } from "../../ServerCalls/profile.mjs";
 import backArrowImg from "../../assets/images/backArrow.png";
 import { isoToVisualFormat } from "../../Utilities/dates.js";
+import ObjectTools from "../../Utilities/ObjectTools.js";
 
 const SocialCourseRoundBox = (props) => {
-    const value = props.value ? props.value : 0;
-    const bg = value < 0 ? "bg-[green]" : value > 0 ? "bg-[red]" : "bg-gray-subtle";
+    const value = props.value ? props.value : "\u2013";
+    const bg = value < 0 ? "bg-[#d0ffb5]" : value > 0 ? "bg-[#ffd2d2]" : "bg-[#eeeeee]";
 
     return (
-        <div className={"inline-block text-[13px] w-[10%] text-center text-white m-[0.5%] py-[2px] " + bg}>
+        <div className={"inline-block text-[18px] w-[10%] text-center text-gray-mild m-[0.5%] py-[2px] " + bg}>
             {value}
         </div>
     );
@@ -16,9 +17,13 @@ const SocialCourseRoundBox = (props) => {
 
 const SocialCourseRound = (props) => {
 
+    const total = ObjectTools.getRoundTotal(props.round);
+    const totalColorBg = total < 0 ? "bg-[green]" : total > 0 ? "bg-[red]" : "bg-gray-subtle";
+
     return (
-        <div className="mt-[10px]">
-            <div className="block w-[100%]">
+        <div className="mt-[25px]">
+            <div className="block w-[100%] mb-[3px]">
+                <div className={"w-[30px] text-white text-center inline-block mr-[5px] " + totalColorBg}>{total}</div>
                 <div className="inline">Round {props.index + 1}</div>
                 <div className="float-right mr-[10px]">{isoToVisualFormat(props.round.date)}</div>
             </div>
@@ -53,7 +58,7 @@ const SocialCourse = (props) => {
     return (
         <div>
             <div>
-                <button className="bg-gray-dark rounded-[7px] border-none h-[35px] align-middle" onClick={props.onBack}>
+                <button className="bg-gray-dark rounded-[7px] border-none h-[35px] align-middle cursor-pointer" onClick={props.onBack}>
                     <img src={backArrowImg} className="w-[25px] h-[25px] mx-[8px]"></img>
                 </button>
                 <div className="inline-block align-middle ml-[8px] text-[20px] text-gray-dark w-[calc(100%-60px)] truncate">{props.course.name}</div>
--- client/src/Jsx/Components/SocialCourseSlot.jsx
+++ client/src/Jsx/Components/SocialCourseSlot.jsx
@@ -2,7 +2,7 @@
 const SocialCourseSlot = (props) => {
 
     return (
-        <button {...props} className="text-left bg-[#c0c0c0] w-[45%] text-black pl-[8px] py-[3px] pr-[5px] m-[3px] rounded-[7px] truncate">
+        <button {...props} className="text-left bg-[#c0c0c0] w-[45%] text-black pl-[8px] py-[3px] pr-[5px] m-[3px] rounded-[7px] truncate cursor-pointer">
             {props.course.name}
         </button>
     );
