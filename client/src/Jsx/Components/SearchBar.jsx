c3a533c71b53c5d6205faed64ed6d176031e0789
Tim Shichalin <shichatim@gmail.com>
2025-10-23 03:14:56 -0700

    search bar extends on focus

--- client/src/Jsx/Components/SearchBar.jsx
+++ client/src/Jsx/Components/SearchBar.jsx
@@ -10,7 +10,7 @@ const SearchBar = (props) => {
     return (
         <div className={props.className}>
             <img src={searchIcon} className="absolute w-[28px] my-[3px] ml-[5px]" alt="Search icon"></img>
-            <input onChange={onChange} id={props.id} name={props.name} className="bg-gray-light rounded-[5px] w-[120px] font-bold h-[34px] text-[18px] pl-[35px]" placeholder="Search">
+            <input onChange={onChange} id={props.id} name={props.name} className="bg-gray-light rounded-[5px] w-[110px] focus:w-[200px] transition-width duration-300 ease-in-out font-bold h-[34px] text-[18px] pl-[35px]" placeholder="Search">
             </input>
         </div>
     );
